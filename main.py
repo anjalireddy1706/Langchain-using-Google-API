2aff91b7745b3987893119add2bb6167511b5876
anjalireddy1706 <57055215+anjalireddy1706@users.noreply.github.com>
2025-05-30 10:09:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import warnings
 import sys
 from constants import API_KEY
 from langchain_google_genai import GoogleGenerativeAI
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import SequentialChain
 import streamlit as st
 
 sys.modules['warnings'] = warnings
@@ -12,10 +16,49 @@ llm = GoogleGenerativeAI(
     google_api_key=API_KEY,
 )
 
-st.title("Langchain Demo with Google AI")
-input_text = st.text_input("Search the topic you want")
+st.title('Celebrity Search Results using Google AI')
+input_text=st.text_input("Name the celebrity you're looking for")
+
+first_input_prompt=PromptTemplate(
+    input_variables=['name'],
+    template="Tell me about celebrity {name}"
+)
+
+# Memory
+
+person_memory = ConversationBufferMemory(input_key='name', memory_key='chat_history')
+dob_memory = ConversationBufferMemory(input_key='person', memory_key='chat_history')
+descr_memory = ConversationBufferMemory(input_key='dob', memory_key='description_history')
+
+chain=LLMChain(
+    llm=llm,prompt=first_input_prompt,verbose=True,output_key='person',memory=person_memory)
+
+# Prompt Templates
+
+second_input_prompt=PromptTemplate(
+    input_variables=['person'],
+    template="when was {person} born"
+)
+
+chain2=LLMChain(
+    llm=llm,prompt=second_input_prompt,verbose=True,output_key='dob',memory=dob_memory)
+# Prompt Templates
+
+third_input_prompt=PromptTemplate(
+    input_variables=['dob'],
+    template="Mention 5 major events happened around {dob} in the world"
+)
+chain3=LLMChain(llm=llm,prompt=third_input_prompt,verbose=True,output_key='description',memory=descr_memory)
+parent_chain=SequentialChain(
+    chains=[chain,chain2,chain3],input_variables=['name'],output_variables=['person','dob','description'],verbose=True)
+
+
 
 if input_text:
-    response = llm(input_text)
-    st.write(response)
+    st.write(parent_chain({'name':input_text}))
 
+    with st.expander('Person Name'): 
+        st.info(person_memory.buffer)
+
+    with st.expander('Major Events'): 
+        st.info(descr_memory.buffer)
